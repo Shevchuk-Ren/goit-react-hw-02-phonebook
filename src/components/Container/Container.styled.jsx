08085b9a9c4e75cd01cb4c144f20118b467dd04d
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-09-06 17:08:50 +0300

    add react app

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  background-color: rgba(39, 36, 36, 0.394);
+  background-color: rgba(98, 93, 93, 0.394);
   padding: 15px;
   width: fit-content;
   border-radius: 6px;
